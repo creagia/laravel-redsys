629e44c9525f702b3ddaac4cfd72ab0521528911
David Torras <davidtorrasceron@gmail.com>
2024-06-02 18:11:10 +0200

    handle redsys responses for POST method

---
+++ src/Actions/HandleRedsysResponse.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Creagia\LaravelRedsys\Actions;
+
+use Creagia\LaravelRedsys\Events\RedsysSuccessfulEvent;
+use Creagia\LaravelRedsys\Events\RedsysUnsuccessfulEvent;
+use Creagia\LaravelRedsys\Exceptions\RedsysRequestNotFound;
+use Creagia\LaravelRedsys\RedsysCard;
+use Creagia\LaravelRedsys\RedsysNotificationLog;
+use Creagia\LaravelRedsys\RedsysRequestStatus;
+use Creagia\LaravelRedsys\Request;
+use Creagia\Redsys\Exceptions\DeniedRedsysPaymentResponseException;
+use Creagia\Redsys\RedsysResponse;
+use Creagia\Redsys\Support\PostRequestError;
+use Illuminate\Support\Str;
+
+class HandleRedsysResponse
+{
+    public function __invoke(
+        ?Request $request,
+        RedsysResponse|PostRequestError $response,
+    )
+    {
+        $redsysNotificationLog = RedsysNotificationLog::create([
+            'merchant_parameters' => $response instanceof PostRequestError
+                ? $response->responseParameters
+                : $response->merchantParametersArray,
+        ]);
+
+        if (! $request) {
+            throw new RedsysRequestNotFound('Redsys Request not found from bank response');
+        }
+
+        $redsysNotificationLog->redsysRequest()->associate($request);
+        $redsysNotificationLog->save();
+
+        if ($response instanceof PostRequestError) {
+            /**
+             * Error
+             */
+            $request->status = RedsysRequestStatus::Error;
+            $request->response_message = $response->message;
+            $request->response_code = $response->code;
+            $request->save();
+
+            return;
+        }
+
+        $request->auth_code = (empty($authCode = trim($response->parameters->responseAuthorisationCode))) ? null : $authCode;
+        $request->response_code = $response->parameters->responseCode ?? null;
+        $request->response_message = $response->parameters->responseDescription;
+
+        try {
+            $notificationData = $response->checkResponse();
+
+            RedsysSuccessfulEvent::dispatch($request, $notificationData->toArray());
+            $request->status = RedsysRequestStatus::Success;
+            $request->save();
+
+            if ($request->model) {
+                $request->model->paidWithRedsys();
+            }
+
+            if (
+                $request->save_card
+                and filled($notificationData->merchantIdentifier)
+                and filled($notificationData->cofTransactionId)
+            ) {
+                $redsysCard = new RedsysCard();
+                $redsysCard->uuid = Str::uuid();
+                $redsysCard->number = $notificationData->cardNumber;
+                $redsysCard->expiration_date = $notificationData->cardExpiryDate;
+                $redsysCard->merchant_identifier = $notificationData->merchantIdentifier;
+                $redsysCard->cof_transaction_id = $notificationData->cofTransactionId;
+
+                if ($request->card_request_model_id) {
+                    $redsysCard->model_id = $request->card_request_model_id;
+                    $redsysCard->model_type = $request->card_request_model_type;
+                }
+
+                $redsysCard->save();
+            }
+        } catch (DeniedRedsysPaymentResponseException $e) {
+            $errorMessage = $e->getMessage();
+            RedsysUnsuccessfulEvent::dispatch($request, $errorMessage);
+
+            $request->status = RedsysRequestStatus::Error;
+            $request->response_message = $errorMessage;
+            $request->save();
+        }
+    }
+}
--- src/Controllers/RedsysNotificationController.php
+++ src/Controllers/RedsysNotificationController.php
@@ -3,6 +3,7 @@
 namespace Creagia\LaravelRedsys\Controllers;
 
 use Creagia\LaravelRedsys\Actions\CreateRedsysClient;
+use Creagia\LaravelRedsys\Actions\HandleRedsysResponse;
 use Creagia\LaravelRedsys\Events\RedsysNotificationEvent;
 use Creagia\LaravelRedsys\Events\RedsysSuccessfulEvent;
 use Creagia\LaravelRedsys\Events\RedsysUnsuccessfulEvent;
@@ -21,6 +22,11 @@ use Illuminate\Support\Str;
 
 class RedsysNotificationController
 {
+    public function __construct(
+        private HandleRedsysResponse $handleRedsysResponse,
+    ) {
+    }
+
     /**
      * @throws DeniedRedsysPaymentResponseException
      * @throws RedsysRequestNotFound
@@ -37,60 +43,10 @@ class RedsysNotificationController
         $redsysResponse = new RedsysResponse($redsysClient);
         $redsysResponse->setParametersFromResponse($inputs);
 
-        $redsysNotificationLog = RedsysNotificationLog::create([
-            'merchant_parameters' => $redsysResponse->merchantParametersArray,
-        ]);
-
         $request = Request::where('order_number', $redsysResponse->parameters->order)->first();
-
-        if (! $request) {
-            throw new RedsysRequestNotFound('Redsys Request not found from bank response');
-        }
-
-        $redsysNotificationLog->redsysRequest()->associate($request);
-        $redsysNotificationLog->save();
-
-        $request->response_code = $redsysResponse->parameters->responseCode ?? null;
-        $request->auth_code = (empty($authCode = trim($redsysResponse->parameters->responseAuthorisationCode))) ? null : $authCode;
-
-        try {
-            $notificationData = $redsysResponse->checkResponse();
-
-            RedsysSuccessfulEvent::dispatch($request, $notificationData->toArray());
-            $request->status = RedsysRequestStatus::Paid;
-
-            $request->save();
-
-            if ($request->model) {
-                $request->model->paidWithRedsys();
-            }
-
-            if (
-                $request->save_card
-                and filled($notificationData->merchantIdentifier)
-                and filled($notificationData->cofTransactionId)
-            ) {
-                $redsysCard = new RedsysCard();
-                $redsysCard->uuid = Str::uuid();
-                $redsysCard->number = $notificationData->cardNumber;
-                $redsysCard->expiration_date = $notificationData->cardExpiryDate;
-                $redsysCard->merchant_identifier = $notificationData->merchantIdentifier;
-                $redsysCard->cof_transaction_id = $notificationData->cofTransactionId;
-
-                if ($request->card_request_model_id) {
-                    $redsysCard->model_id = $request->card_request_model_id;
-                    $redsysCard->model_type = $request->card_request_model_type;
-                }
-
-                $redsysCard->save();
-            }
-        } catch (DeniedRedsysPaymentResponseException $e) {
-            $errorMessage = $e->getMessage();
-            RedsysUnsuccessfulEvent::dispatch($request, $errorMessage);
-
-            $request->status = RedsysRequestStatus::Denied;
-            $request->response_message = $errorMessage;
-            $request->save();
-        }
+        ($this->handleRedsysResponse)(
+            request: $request,
+            response: $redsysResponse,
+        );
     }
 }
