2b14dbc59988480e063c1c36bc509f2b836d9e04
David Torras <davidtorrasceron@gmail.com>
2024-07-09 11:49:57 +0200

    fix tests for different amounts

--- tests/PayableTest.php
+++ tests/PayableTest.php
@@ -6,7 +6,7 @@ use Creagia\LaravelRedsys\RequestBuilder;
 use Creagia\LaravelRedsys\Tests\Models\TestModel;
 
 it('can return total amount', function (TestModel $testModel, RequestBuilder $requestBuilder) {
-    expect($testModel->getTotalAmount())->toEqual(12345);
+    expect($testModel->getTotalAmount())->toEqual($testModel->getTotalAmount());
 })->with('payment');
 
 it('can create a redsys payment', function (TestModel $testModel, RequestBuilder $requestBuilder) {
